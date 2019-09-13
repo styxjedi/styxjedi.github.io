2b943e1acaff5f27bb4856a00bd3ed070726a9d1
Cunliang Kong <styxjedi@outlook.com>
2019-09-13 10:02:47 +0800

    Site updated: 2019-09-13 10:02:46

--- js/scroll-cookie.js
+++ js/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sat Aug 24 2019 19:00:59 GMT+0800 (CST)
+// build time:Fri Sep 13 2019 10:02:45 GMT+0800 (CST)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
